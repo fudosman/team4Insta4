48be8d9c005dcb0238fb06396bf7688d2a27a642
Chidimma Dominic Nweke <realchidinweke@gmail.com>
2022-02-02 14:46:13 +0100

    Added User Model

--- models/users.js
+++ models/users.js
@@ -3,35 +3,42 @@ const mongoose = require("mongoose");
 const userSchema = mongoose.Schema(
   {
     username: {
-      type: String,
-      required: true,
+      type: String
     },
+
     firstname: {
       type: String,
       required: true,
     },
-    surname: {
+
+    lastname: {
       type: String,
       required: true,
     },
+
+    profilePicture: {
+      type: String
+    },
+
     email: {
       type: String,
       required: true,
       unique: true,
+      //An alternative to regex to perform email validation
+      validate: [validator.isEmail, 'Please Provide a valid email'],
     },
+
     password: {
-      type: Number,
-      required: true,
+      hash: {
+        type: String,
+        required: true
+      },
+      salt: String
     },
-    posts: {
-      // here i want to get a reference to the posts of this user only
-      
-    },
-    feeds: {
-      // here i want to get a reference to the posts of the user and all those he is following
-      
-    },
+
+    feed: []
   },
+
   {
     timestamps: {
       createdAt: "created_at",
@@ -39,5 +46,12 @@ const userSchema = mongoose.Schema(
     },
   }
 );
+
+
+//INSTANCE METHODS
+userSchema.methods.setPassword = (pwd) => {
+  this.password.salt = await bcrypt.genSalt(10);
+  this.password.hash = await bcrypt.hash(pwd, this.password.salt);
+};
 
 module.exports = mongoose.model("users", userSchema);
